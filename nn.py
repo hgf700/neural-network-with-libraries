b7e3e26dd70daa14c3e198d6e482275cdd501de4
hgf700 <pawellapinski33@gmail.com>
2025-03-17 15:35:04 +0100

    feat: 1 lession knn for iris with background colors

---
+++ nn.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
